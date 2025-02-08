a5bc57041f52dd5962cae01316a875bc4261bd27
Prabh ‪‪ <singh20prabh@gmail.com>
2025-02-08 13:34:08 +0530

    layout added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./_auth/AuthLayout";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import RootLayout from "./_root/RootLayout";
+import { Home } from "./_root/pages";
+import "./global.css";
+
 const App = () => {
-  return <div className="bg-blue-500 text-white p-4">App</div>;
+  return (
+    <main className=" flex h-screen ">
+      <Routes>
+        {/* public */}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/* private */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
 };
 
 export default App;
